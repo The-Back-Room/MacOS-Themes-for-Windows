6e128059fbbe31aaf89a055f73f2bb03ae635e2d
Joshua Lewis <Darkwater409@gmail.com>
2025-05-09 00:45:34 -0700

    ♻️

--- UNINSTALL.bat
+++ UNINSTALL.bat
@@ -3,12 +3,42 @@
 SET FILE_PATH="%SYSTEMROOT%\Resources\Themes"
 
 ECHO. 
-ECHO ::::::::::::::::::::::::::::::::::::::::::
-ECHO ::        DELETING THEME FILES          ::
-ECHO ::::::::::::::::::::::::::::::::::::::::::
+ECHO ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ECHO ::                DELETING THEME FILES                  ::
+ECHO ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ECHO.
 
-DEL /F "%FILE_PATH%\BigSur (dark).theme"
+ECHO DEL /F "%FILE_PATH%\BigSur (dark).theme"
+ECHO DEL /F "%FILE_PATH%\BigSur Colorful (dark).theme"
+ECHO DEL /F "%FILE_PATH%\BigSur Colorful.theme"
+ECHO DEL /F "%FILE_PATH%\BigSur.theme"
+ECHO DEL /F "%FILE_PATH%\Catalina.theme"
+ECHO DEL /F "%FILE_PATH%\Catalina (dark).theme"
+ECHO DEL /F "%FILE_PATH%\El Capitan.theme"
+ECHO DEL /F "%FILE_PATH%\High Sierra.theme"
+ECHO DEL /F "%FILE_PATH%\Jaguar.theme"
+ECHO DEL /F "%FILE_PATH%\Leopard.theme"
+ECHO DEL /F "%FILE_PATH%\Lion.theme"
+ECHO DEL /F "%FILE_PATH%\Mavericks.theme"
+ECHO DEL /F "%FILE_PATH%\Mojave (dark).theme"
+ECHO DEL /F "%FILE_PATH%\Mojave.theme"
+ECHO DEL /F "%FILE_PATH%\Monterey (dark).theme"
+ECHO DEL /F "%FILE_PATH%\Monterey.theme"
+ECHO DEL /F "%FILE_PATH%\Mountain Lion.theme"
+ECHO DEL /F "%FILE_PATH%\Panther.theme"
+ECHO DEL /F "%FILE_PATH%\Puma.theme"
+ECHO DEL /F "%FILE_PATH%\Sequoia (dark).theme"
+ECHO DEL /F "%FILE_PATH%\Sequoia.theme"
+ECHO DEL /F "%FILE_PATH%\Sierra.theme"
+ECHO DEL /F "%FILE_PATH%\Snow Leopard.theme"
+ECHO DEL /F "%FILE_PATH%\Sonoma (dark).theme"
+ECHO DEL /F "%FILE_PATH%\Sonoma.theme"
+ECHO DEL /F "%FILE_PATH%\Tiger.theme"
+ECHO DEL /F "%FILE_PATH%\Ventura (dark).theme"
+ECHO DEL /F "%FILE_PATH%\Ventura.theme"
+ECHO DEL /F "%FILE_PATH%\Yosemite.theme"
+
+
 DEL /F "%FILE_PATH%\BigSur Colorful (dark).theme"
 DEL /F "%FILE_PATH%\BigSur Colorful.theme"
 DEL /F "%FILE_PATH%\BigSur.theme"
@@ -37,4 +67,11 @@ DEL /F "%FILE_PATH%\Tiger.theme"
 DEL /F "%FILE_PATH%\Ventura (dark).theme"
 DEL /F "%FILE_PATH%\Ventura.theme"
 DEL /F "%FILE_PATH%\Yosemite.theme"
+
+ECHO. 
+ECHO ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ECHO ::                      FINISHED                        ::
+ECHO ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ECHO.
+
 PAUSE
