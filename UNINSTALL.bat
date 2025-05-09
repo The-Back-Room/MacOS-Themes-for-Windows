12f9f485a6b9333e12105475532dbe06689e465d
Joshua Lewis <Darkwater409@gmail.com>
2025-05-09 00:15:47 -0700

    🆙🆕
    - Create quick uninstaller

---
+++ UNINSTALL.bat
@@ -0,0 +1,40 @@
+@ECHO OFF
+
+SET FILE_PATH="%SYSTEMROOT%\Resources\Themes"
+
+ECHO. 
+ECHO ::::::::::::::::::::::::::::::::::::::::::
+ECHO ::        DELETING THEME FILES          ::
+ECHO ::::::::::::::::::::::::::::::::::::::::::
+ECHO.
+
+DEL /F "%FILE_PATH%\BigSur (dark).theme"
+DEL /F "%FILE_PATH%\BigSur Colorful (dark).theme"
+DEL /F "%FILE_PATH%\BigSur Colorful.theme"
+DEL /F "%FILE_PATH%\BigSur.theme"
+DEL /F "%FILE_PATH%\Catalina.theme"
+DEL /F "%FILE_PATH%\Catalina (dark).theme"
+DEL /F "%FILE_PATH%\El Capitan.theme"
+DEL /F "%FILE_PATH%\High Sierra.theme"
+DEL /F "%FILE_PATH%\Jaguar.theme"
+DEL /F "%FILE_PATH%\Leopard.theme"
+DEL /F "%FILE_PATH%\Lion.theme"
+DEL /F "%FILE_PATH%\Mavericks.theme"
+DEL /F "%FILE_PATH%\Mojave (dark).theme"
+DEL /F "%FILE_PATH%\Mojave.theme"
+DEL /F "%FILE_PATH%\Monterey (dark).theme"
+DEL /F "%FILE_PATH%\Monterey.theme"
+DEL /F "%FILE_PATH%\Mountain Lion.theme"
+DEL /F "%FILE_PATH%\Panther.theme"
+DEL /F "%FILE_PATH%\Puma.theme"
+DEL /F "%FILE_PATH%\Sequoia (dark).theme"
+DEL /F "%FILE_PATH%\Sequoia.theme"
+DEL /F "%FILE_PATH%\Sierra.theme"
+DEL /F "%FILE_PATH%\Snow Leopard.theme"
+DEL /F "%FILE_PATH%\Sonoma (dark).theme"
+DEL /F "%FILE_PATH%\Sonoma.theme"
+DEL /F "%FILE_PATH%\Tiger.theme"
+DEL /F "%FILE_PATH%\Ventura (dark).theme"
+DEL /F "%FILE_PATH%\Ventura.theme"
+DEL /F "%FILE_PATH%\Yosemite.theme"
+PAUSE
